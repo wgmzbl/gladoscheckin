996fe9d8ae8cc6b45132127d653d5d8e63e51e7f
wgmzbl <64002380+wgmzbl@users.noreply.github.com>
2023-08-22 17:50:54 +0800

    Update checkin.py

--- checkin.py
+++ checkin.py
@@ -16,7 +16,7 @@ def start():
     url= "https://glados.rocks/api/user/checkin"
     url2= "https://glados.rocks/api/user/status"
     referer = 'https://glados.rocks/console/checkin'
-    checkin = requests.post(url,headers={'cookie': cookie ,'referer': referer })
+    checkin = requests.post(url,headers={'cookie': cookie ,'referer': referer }, body={"token":"glados.one"})
     state =  requests.get(url2,headers={'cookie': cookie ,'referer': referer})
    # print(res)
 
